8871e102353f2101da7b0a813c5788ec2fe87020
longdeptrai <longgamer998@gmail.com>
2026-01-05 21:26:10 +0700

    Update remove.bat

--- remove.bat
+++ remove.bat
@@ -1,11 +1,1 @@
-cd C:\Users\longg\Downloads
 
-git clone https://github.com/longgamer998-code/SourceBloxFruit.git
-cd SourceBloxFruit
-
-git pull --rebase
-
-git rm *.lua
-git commit -m "Remove Lua files"
-
-git push
