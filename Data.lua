c69b296b68c702de832d413609aa6d04001c3f31
longdeptrai <longgamer998@gmail.com>
2026-01-07 01:33:33 +0700

    Update Data.lua

--- Data.lua
+++ Data.lua
@@ -1,47 +1,64 @@
 --[[
-    DATA MODULE - Chứa thông tin đảo/mob theo Sea và Level
+    DATA MODULE - Tên mob chính xác từ Blox Fruits Wiki
 ]]
 
 local Data = {}
 
 Data.Sea1 = {
-    {Island = "Starter Island", Level = {1, 15}, Mob = "Bandit", QuestNPC = "Bandit Quest Giver", MobPos = Vector3.new(-1124, 15, 3855), NPCPos = Vector3.new(-1139, 15, 3824)},
-    {Island = "Jungle", Level = {15, 30}, Mob = "Monkey", QuestNPC = "Jungle Quest Giver", MobPos = Vector3.new(-1613, 42, 152), NPCPos = Vector3.new(-1603, 36, 154)},
-    {Island = "Pirate Village", Level = {30, 60}, Mob = "Pirate", QuestNPC = "Pirate Quest Giver", MobPos = Vector3.new(-1140, 5, 3830), NPCPos = Vector3.new(-1133, 5, 3856)},
-    {Island = "Desert", Level = {60, 90}, Mob = "Desert Bandit", QuestNPC = "Desert Quest Giver", MobPos = Vector3.new(1095, 6, 4180), NPCPos = Vector3.new(1090, 6, 4210)},
-    {Island = "Frozen Village", Level = {90, 120}, Mob = "Snow Bandit", QuestNPC = "Frozen Quest Giver", MobPos = Vector3.new(1386, 87, -1298), NPCPos = Vector3.new(1360, 87, -1310)},
-    {Island = "Marine Fortress", Level = {120, 150}, Mob = "Marine", QuestNPC = "Marine Quest Giver", MobPos = Vector3.new(-4914, 50, 4281), NPCPos = Vector3.new(-4890, 50, 4300)},
-    {Island = "Skylands", Level = {150, 190}, Mob = "Sky Bandit", QuestNPC = "Sky Quest Giver", MobPos = Vector3.new(-4869, 717, -2623), NPCPos = Vector3.new(-4850, 717, -2610)},
-    {Island = "Prison", Level = {190, 275}, Mob = "Prisoner", QuestNPC = "Prison Quest Giver", MobPos = Vector3.new(5765, 5, 747), NPCPos = Vector3.new(5740, 5, 760)},
-    {Island = "Colosseum", Level = {275, 350}, Mob = "Gladiator", QuestNPC = "Colosseum Quest Giver", MobPos = Vector3.new(-1428, 7, -3014), NPCPos = Vector3.new(-1450, 7, -3000)},
-    {Island = "Magma Village", Level = {350, 450}, Mob = "Military Soldier", QuestNPC = "Magma Quest Giver", MobPos = Vector3.new(-5250, 12, 8515), NPCPos = Vector3.new(-5230, 12, 8530)},
-    {Island = "Underwater City", Level = {450, 575}, Mob = "Fishman", QuestNPC = "Underwater Quest Giver", MobPos = Vector3.new(61163, 11, 1819), NPCPos = Vector3.new(61140, 11, 1830)},
-    {Island = "Fountain City", Level = {575, 700}, Mob = "Galley Pirate", QuestNPC = "Fountain Quest Giver", MobPos = Vector3.new(5127, 38, 4046), NPCPos = Vector3.new(5110, 38, 4060)},
+    {Island = "Starter Island", Level = {1, 10}, Mob = "Bandit"},
+    {Island = "Jungle", Level = {10, 20}, Mob = "Monkey"},
+    {Island = "Jungle", Level = {20, 30}, Mob = "Gorilla"},
+    {Island = "Pirate Village", Level = {30, 45}, Mob = "Pirate"},
+    {Island = "Pirate Village", Level = {45, 60}, Mob = "Brute"},
+    {Island = "Desert", Level = {60, 75}, Mob = "Desert Bandit"},
+    {Island = "Desert", Level = {75, 90}, Mob = "Desert Officer"},
+    {Island = "Frozen Village", Level = {90, 105}, Mob = "Snow Bandit"},
+    {Island = "Frozen Village", Level = {105, 120}, Mob = "Snowman"},
+    {Island = "Marine Fortress", Level = {120, 135}, Mob = "Chief Petty Officer"},
+    {Island = "Marine Fortress", Level = {135, 150}, Mob = "Vice Admiral"},
+    {Island = "Skylands", Level = {150, 175}, Mob = "Sky Bandit"},
+    {Island = "Skylands", Level = {175, 200}, Mob = "Dark Master"},
+    {Island = "Prison", Level = {200, 235}, Mob = "Prisoner"},
+    {Island = "Prison", Level = {235, 275}, Mob = "Dangerous Prisoner"},
+    {Island = "Colosseum", Level = {275, 300}, Mob = "Toga Warrior"},
+    {Island = "Colosseum", Level = {300, 350}, Mob = "Gladiator"},
+    {Island = "Magma Village", Level = {350, 375}, Mob = "Military Soldier"},
+    {Island = "Magma Village", Level = {375, 400}, Mob = "Military Spy"},
+    {Island = "Underwater City", Level = {400, 450}, Mob = "Fishman Warrior"},
+    {Island = "Underwater City", Level = {450, 500}, Mob = "Fishman Commando"},
+    {Island = "Skylands Upper", Level = {500, 550}, Mob = "God's Guard"},
+    {Island = "Skylands Upper", Level = {550, 575}, Mob = "Shanda"},
+    {Island = "Fountain City", Level = {575, 625}, Mob = "Galley Pirate"},
+    {Island = "Fountain City", Level = {625, 700}, Mob = "Galley Captain"},
 }
 
 Data.Sea2 = {
-    {Island = "Kingdom of Rose", Level = {700, 850}, Mob = "Swan Pirate", QuestNPC = "Rose Quest Giver", MobPos = Vector3.new(2179, 73, -6741), NPCPos = Vector3.new(2160, 73, -6720)},
-    {Island = "Green Zone", Level = {850, 950}, Mob = "Plant", QuestNPC = "Green Quest Giver", MobPos = Vector3.new(-2450, 73, -3292), NPCPos = Vector3.new(-2430, 73, -3280)},
-    {Island = "Graveyard", Level = {950, 1050}, Mob = "Zombie", QuestNPC = "Graveyard Quest Giver", MobPos = Vector3.new(-5765, 210, -782), NPCPos = Vector3.new(-5740, 210, -770)},
-    {Island = "Snow Mountain", Level = {1050, 1150}, Mob = "Snowman", QuestNPC = "Snow Quest Giver", MobPos = Vector3.new(613, 400, -5765), NPCPos = Vector3.new(630, 400, -5750)},
-    {Island = "Hot and Cold", Level = {1150, 1250}, Mob = "Hot/Cold Warrior", QuestNPC = "Hot Cold Quest Giver", MobPos = Vector3.new(-6006, 15, -4765), NPCPos = Vector3.new(-5990, 15, -4750)},
-    {Island = "Cursed Ship", Level = {1250, 1350}, Mob = "Cursed Pirate", QuestNPC = "Cursed Quest Giver", MobPos = Vector3.new(923, 125, 32988), NPCPos = Vector3.new(940, 125, 33000)},
-    {Island = "Ice Castle", Level = {1350, 1450}, Mob = "Ice Viking", QuestNPC = "Ice Quest Giver", MobPos = Vector3.new(5668, 27, -6484), NPCPos = Vector3.new(5650, 27, -6470)},
-    {Island = "Forgotten Island", Level = {1450, 1525}, Mob = "Jungle Pirate", QuestNPC = "Forgotten Quest Giver", MobPos = Vector3.new(-3053, 237, -10547), NPCPos = Vector3.new(-3030, 237, -10530)},
+    {Island = "Kingdom of Rose", Level = {700, 775}, Mob = "Swan Pirate"},
+    {Island = "Kingdom of Rose", Level = {775, 850}, Mob = "Factory Staff"},
+    {Island = "Green Zone", Level = {850, 925}, Mob = "Zombie"},
+    {Island = "Green Zone", Level = {925, 1000}, Mob = "Vampire"},
+    {Island = "Graveyard", Level = {1000, 1050}, Mob = "Snow Trooper"},
+    {Island = "Snow Mountain", Level = {1050, 1100}, Mob = "Winter Warrior"},
+    {Island = "Hot and Cold", Level = {1100, 1175}, Mob = "Lab Subordinate"},
+    {Island = "Hot and Cold", Level = {1175, 1250}, Mob = "Horned Warrior"},
+    {Island = "Cursed Ship", Level = {1250, 1325}, Mob = "Ship Deckhand"},
+    {Island = "Cursed Ship", Level = {1325, 1375}, Mob = "Ship Engineer"},
+    {Island = "Ice Castle", Level = {1375, 1425}, Mob = "Arctic Warrior"},
+    {Island = "Ice Castle", Level = {1425, 1500}, Mob = "Snow Lurker"},
 }
 
 Data.Sea3 = {
-    {Island = "Port Town", Level = {1500, 1575}, Mob = "Pirate Millionaire", QuestNPC = "Port Quest Giver", MobPos = Vector3.new(-285, 44, 5318), NPCPos = Vector3.new(-270, 44, 5330)},
-    {Island = "Hydra Island", Level = {1575, 1675}, Mob = "Hydra Warrior", QuestNPC = "Hydra Quest Giver", MobPos = Vector3.new(5228, 452, 344), NPCPos = Vector3.new(5225, 452, 340)},
-    {Island = "Great Tree", Level = {1700, 1775}, Mob = "Tree Fighter", QuestNPC = "Tree Quest Giver", MobPos = Vector3.new(2275, 30, -6507), NPCPos = Vector3.new(2270, 30, -6505)},
-    {Island = "Floating Turtle", Level = {1775, 1900}, Mob = "Marine Commodore", QuestNPC = "Turtle Quest Giver", MobPos = Vector3.new(-13274, 457, -7735), NPCPos = Vector3.new(-13270, 457, -7730)},
-    {Island = "Haunted Castle", Level = {1975, 2075}, Mob = "Demonic Soul", QuestNPC = "Haunted Quest Giver", MobPos = Vector3.new(-9515, 171, 5765), NPCPos = Vector3.new(-9510, 171, 5760)},
-    {Island = "Sea of Treats", Level = {2075, 2275}, Mob = "Cake Guard", QuestNPC = "Treats Quest Giver", MobPos = Vector3.new(-68, 41, 11514), NPCPos = Vector3.new(-65, 41, 11510)},
-    {Island = "Tiki Outpost", Level = {2225, 2300}, Mob = "Tiki Warrior", QuestNPC = "Tiki Quest Giver", MobPos = Vector3.new(-1057, 65, -10580), NPCPos = Vector3.new(-1055, 65, -10575)},
-    {Island = "Kitsune Shrine", Level = {2300, 2400}, Mob = "Kitsune Guardian", QuestNPC = "Kitsune Quest Giver", MobPos = Vector3.new(-5765, 215, -535), NPCPos = Vector3.new(-5760, 215, -530)},
+    {Island = "Port Town", Level = {1500, 1575}, Mob = "Pirate Millionaire"},
+    {Island = "Hydra Island", Level = {1575, 1675}, Mob = "Dragon Crew Warrior"},
+    {Island = "Great Tree", Level = {1675, 1750}, Mob = "Forest Pirate"},
+    {Island = "Floating Turtle", Level = {1750, 1900}, Mob = "Fishman Raider"},
+    {Island = "Haunted Castle", Level = {1900, 2000}, Mob = "Demonic Soul"},
+    {Island = "Sea of Treats", Level = {2000, 2100}, Mob = "Cake Guard"},
+    {Island = "Sea of Treats", Level = {2100, 2200}, Mob = "Cookie Crafter"},
+    {Island = "Tiki Outpost", Level = {2200, 2300}, Mob = "Tiki Warrior"},
+    {Island = "Kitsune Shrine", Level = {2300, 2400}, Mob = "Kitsune Guardian"},
 }
 
--- Lấy data đảo phù hợp với level
 function Data.GetIslandByLevel(level, sea)
     local seaData = Data["Sea" .. sea]
     if not seaData then return nil end
@@ -52,21 +69,8 @@ function Data.GetIslandByLevel(level, sea)
         end
     end
     
-    -- Nếu level cao hơn max, trả về đảo cuối
+    -- Nếu level cao hơn max, trả về entry cuối
     return seaData[#seaData]
-end
-
--- Lấy đảo tiếp theo
-function Data.GetNextIsland(currentIsland, sea)
-    local seaData = Data["Sea" .. sea]
-    if not seaData then return nil end
-    
-    for i, island in ipairs(seaData) do
-        if island.Island == currentIsland then
-            return seaData[i + 1]
-        end
-    end
-    return nil
 end
 
 return Data
